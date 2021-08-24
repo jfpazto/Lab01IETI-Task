9481f7536572a85ce584211209958a96638ac077
jfpazto <jfpazto@hotmail.com>
2021-08-24 13:14:10 +0100

    Primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Lab01IETI-Task
---
+++ src/main/java/com/escuelaing/Lab012/Lab012Application.java
@@ -0,0 +1,13 @@
+package com.escuelaing.Lab012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab012Application.class, args);
+	}
+
+}
